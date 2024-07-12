10df1e9fc6b783aeb5f92af81e00ebdde88729d8
Simon K <jackofspaces@gmail.com>
2024-07-12 18:34:17 +0100

    more `producer` basics; fixing docker mishaps

--- internal/producer/producer.go
+++ internal/producer/producer.go
@@ -1,6 +1,11 @@
 package producer
 
-import "github.com/confluentinc/confluent-kafka-go/kafka"
+import (
+	"fmt"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
 
 type Producer interface {
 	Produce()
@@ -16,12 +21,51 @@ func New(cfg *kafka.ConfigMap) *SimpleProducer {
 	if err != nil {
 		panic(err)
 	}
-	return &SimpleProducer{producer: prod}
+	s := &SimpleProducer{producer: prod}
+	go s.Monitor()
+	return s
 }
 
 // Produce dispatches a new kafka message to the
 // configured topic
-func (s *SimpleProducer) Produce(message *kafka.Message) {
+func (s *SimpleProducer) Produce(message *kafka.Message) error {
+	err := s.producer.Produce(message, nil)
+	if err != nil {
+		if err.(kafka.Error).Code() == kafka.ErrQueueFull {
+			time.Sleep(time.Second)
+		}
+		fmt.Printf("Failed to produce message: %v\n", err)
+	}
+	return err
+}
+
+func (s *SimpleProducer) Wait() {
+	for s.producer.Flush(10_000) > 0 {
+		fmt.Print("still waiting to flush all messages from the producer")
+
+	}
+}
+
+// Monitor listens to the producers
+func (s *SimpleProducer) Monitor() {
+	for event := range s.producer.Events() {
+		switch eventType := event.(type) {
+		case *kafka.Message:
+			// the message delivery report, indicating success or
+			// permanent failure after retries have been exhausted.
+			message := eventType
+			if message.TopicPartition.Error != nil {
+				fmt.Printf("Delivery failed: %v\n", message.TopicPartition.Error)
+			}
+			fmt.Printf("Delivered message to topic %s, partition: [%d] at offset: %v\n", *message.TopicPartition.Topic, message.TopicPartition.Partition, message.TopicPartition.Offset)
+		case kafka.Error:
+			// client instance-level errors
+			// these are auto retried and should be considered informational
+			fmt.Printf("Error: %v\n", eventType)
+		default:
+			fmt.Printf("Ignored event: %s\n", eventType)
+		}
+	}
 
 }
 
--- main.go
+++ main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/symonk/kafka-demo/internal/consumer"
+	"github.com/symonk/kafka-demo/internal/models"
 	"github.com/symonk/kafka-demo/internal/producer"
 )
 
 func main() {
-	fmt.Println("in main")
-	cfg := kafka.ConfigMap{"bootstrap.servers": "localhost"}
-	prod := producer.New(&cfg)
-	defer prod.Stop()
-	consumer := consumer.New(&cfg)
+	topic := "heroes"
+	prodCfg := kafka.ConfigMap{"bootstrap.servers": "localhost"}
+	consumerCfg := kafka.ConfigMap{"bootstrap.servers": "localhost", "group.id": "demo"}
+	producer := producer.New(&prodCfg)
+	defer producer.Stop()
+	consumer := consumer.New(&consumerCfg)
 	defer consumer.Stop()
+
+	for i := range 10000 {
+		value := models.Superhero{Id: i, Name: fmt.Sprintf("name%d", i), Power: i * 100, Melee: false}
+		bs, err := json.Marshal(value)
+		if err != nil {
+			panic(err)
+		}
+
+		err = producer.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          bs,
+			Headers:        []kafka.Header{{Key: "demo-header", Value: []byte("demo")}},
+		})
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	producer.Wait()
 }
